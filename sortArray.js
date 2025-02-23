225b02c38d7fb0860c5ffd7af9f721fb0e1225fa
yashwardhanjaiswal <jaiswalyashwardhan20@gmail.com>
2025-02-23 11:14:40 +0530

    fix sorting

--- sortArray.js
+++ sortArray.js
@@ -9,22 +9,25 @@ const arr = [
   { name: "priya" },
 ];
 
-arr.sort((a, b) => {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
-  return 0;
-});
+// arr.sort((a, b) => {
+//   if (a.name < b.name) return -1;
+//   if (a.name > b.name) return 1;
+//   return 0;
+// });
+
+//alternative solution
+arr.sort((a, b) => b.name.localeCompare(a.name));
 // console.log(arr);
 
 //2.sort array based on lastSeen.
 const data = [
-    { id: 2, name: 'Bob', lastSeen: '2024-11-29T22:15:00Z' },
-    { id: 3, name: 'Charlie', lastSeen: '2024-11-27T16:45:00Z' },
-    { id: 1, name: 'Alice', lastSeen: '2024-11-28T14:30:00Z' },
-    { id: 4, name: 'Diana', lastSeen: '2024-11-28T09:15:00Z' }
-  ]
-  
-  data.sort((a, b) => {
-     new Date(a.lastSeen) - new Date(b.lastSeen);
-  })
-  console.log(data, 'data');
+  { id: 2, name: "Bob", lastSeen: "2024-11-29T22:15:00Z" },
+  { id: 3, name: "Charlie", lastSeen: "2024-11-27T16:45:00Z" },
+  { id: 1, name: "Alice", lastSeen: "2024-11-28T14:30:00Z" },
+  { id: 4, name: "Diana", lastSeen: "2024-11-28T09:15:00Z" },
+];
+
+data.sort((a, b) => {
+  new Date(a.lastSeen).getTime() - new Date(b.lastSeen).getTime();
+});
+console.log(data, "data");
